e47a3679fbe99a1576d130151cc4eba86fb0fe4f
Eddy Savri <masterneo974@me.com>
2017-11-26 00:15:35 +0100

    user interface ok

--- src/videoDetail.js
+++ src/videoDetail.js
@@ -4,9 +4,12 @@ class VideoDetail extends Component {
   render(){
     return(
       <div className='col-md-8'>
-        <img width='750px' src='https://www.wired.com/wp-content/uploads/2015/03/youtube_fail.jpg'/>
+        <div className='embed-responsive embed-responsive-16by9'>
+          <iframe className='embed-responsive-item' src='https://www.youtube.com/embed/1G4isv_Fylg'></iframe>
+        </div>
         <h3> Title of the Video </h3>
         <p> Lorem ipsum dolor sit amet, omnium voluptua posidonium sit an, iudico timeam mandamus et pri, quo in laoreet platonem recteque. Per ferri conceptam percipitur ei, velit ridens ius ei. Vim duis aliquip te. At nam iudicabit constituam, magna ceteros definiebas cu nam, etiam habemus antiopam vis id.</p>
+
       </div>
 
     );
